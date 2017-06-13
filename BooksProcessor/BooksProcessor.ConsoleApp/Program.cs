4221a0a0f5d253b3be212e0381c64782b1a801d4
Yoelvis Mulen <ymulenll@hotmail.com>
2017-06-13 00:04:01 -0300

    used unity ioc

--- BooksProcessor/BooksProcessor.ConsoleApp/Program.cs
+++ BooksProcessor/BooksProcessor.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using BooksProcessor.LiteDB;
+﻿using BooksProcessor.Interfaces;
+using BooksProcessor.LiteDB;
+using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,15 +15,29 @@ namespace BooksProcessor.ConsoleApp
         static void Main(string[] args)
         {
             // Composition using poor's man dependency injection.
-            var booksDataProvider = new StreamBooksDataProvider();
-            var logger = new ConsoleLogger();
-            var booksValidator = new SimpleBooksValidator(logger);
-            var booksMapper = new SimpleBooksMapper();
-            var booksParser = new SimpleBooksParser(booksValidator, booksMapper);
-            var booksStorage = new LiteDBBooksStorage(logger);
-            var booksProcessor = new BooksProcessor(booksDataProvider, booksParser, booksStorage);
-            booksProcessor.ProcessBooks();
+            //var booksDataProvider = new StreamBooksDataProvider();
+            //var logger = new ConsoleLogger();
+            //var booksValidator = new SimpleBooksValidator(logger);
+            //var booksMapper = new SimpleBooksMapper();
+            //var booksParser = new SimpleBooksParser(booksValidator, booksMapper);
+            //var booksStorage = new LiteDBBooksStorage(logger);
+            //var booksProcessor = new BooksProcessor(booksDataProvider, booksParser, booksStorage);
+            //booksProcessor.ProcessBooks();
 
+            using (var container = new UnityContainer())
+            {
+                container.RegisterType<IBooksDataProvider, StreamBooksDataProvider>();
+                container.RegisterType<ILogger, ConsoleLogger>();
+                container.RegisterType<IBooksValidator, SimpleBooksValidator>();
+                container.RegisterType<IBooksMapper, SimpleBooksMapper>();
+                container.RegisterType<IBooksParser, SimpleBooksParser>();
+                container.RegisterType<IBooksStorage, LiteDBBooksStorage>();
+                container.RegisterType<BooksProcessor>();
+
+                var booksProcessor = container.Resolve<BooksProcessor>();
+                booksProcessor.ProcessBooks();
+            }
+            
             Console.ReadKey();
         }
     }
