2f20bf5979d0cab7fd42963479fed1e7b5413a41
Yoelvis Mulen <ymulenll@hotmail.com>
2017-06-14 00:49:05 -0300

    Minor changes in composition root.

--- BooksProcessor/BooksProcessor.ConsoleApp/Program.cs
+++ BooksProcessor/BooksProcessor.ConsoleApp/Program.cs
@@ -12,6 +12,18 @@ namespace BooksProcessor.ConsoleApp
     {
         static void Main(string[] args)
         {
+            //var booksStream = Assembly.GetAssembly(typeof(BooksProcessor)).GetManifestResourceStream("BooksProcessor.NewBooks.txt");
+
+            //var booksProcessor = new BooksProcessor();
+            //booksProcessor.ProcessBooks(booksStream);
+
+            ComposeDependencies();
+
+            Console.ReadKey();
+        }
+
+        private static void ComposeDependencies()
+        {
             // Composition using poor's man dependency injection.
             var booksDataProvider = new StreamBooksDataProvider();
             var logger = new ConsoleLogger();
@@ -21,8 +33,6 @@ namespace BooksProcessor.ConsoleApp
             var booksStorage = new LiteDBBooksStorage(logger);
             var booksProcessor = new BooksProcessor(booksDataProvider, booksParser, booksStorage);
             booksProcessor.ProcessBooks();
-
-            Console.ReadKey();
         }
     }
 }
